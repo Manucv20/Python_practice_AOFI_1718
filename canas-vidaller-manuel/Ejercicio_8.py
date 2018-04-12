ab5b5458509c0a6db4c11e4fe679224e7c42b2e4
Manucv20 <manusevilla20@gmail.com>
2018-04-12 20:40:21 +0200

    añado el ejercicio8

--- canas-vidaller-manuel/Ejercicio_8.py
+++ canas-vidaller-manuel/Ejercicio_8.py
@@ -1,56 +1,18 @@
-#Creo e inicializo las variables
 import time
-almuerzo1 = ""
-almuerzo2 = ""
-almuerzo3 = ""
-almuerzo4 = ""
-almuerzo5 = ""
-almuerzo6 = ""
-almuerzo7 = ""
-cena1 = ""
-cena2 = ""
-cena3 = ""
-cena4 = ""
-cena5 = ""
-cena6 = ""
-cena7 = ""
-dia1 = "Lunes"
-dia2 = "Martes"
-dia3 = "Miercoles"
-dia4 = "Jueves"
-dia5 = "Viernes"
-dia6 = "Sabado"
-dia7 = "Domingo"
-print("Vamos a hacer un menu de almuerzo y cenas para toda la semana")
-almuerzo1 = input("Que vas a querer para almorzar el lunes:\n")
-cena1 = input("Que vas a querer para cenar el lunes:\n")
-almuerzo2 = input("Que vas a querer para almorzar el martes:\n")
-cena2 = input("Que vas a querer para cenar el martes:\n")
-almuerzo3 = input("Que vas a querer para almorzar el miercoles:\n")
-cena3 = input("Que vas a querer para cenar el miercoles:\n")
-almuerzo4 = input("Que vas a querer para almorzar el jueves:\n")
-cena4 = input("Que vas a querer para cenar el jueves:\n")
-almuerzo5 = input("Que vas a querer para almorzar el viernes:\n")
-cena5 = input("Que vas a querer para cenar el viernes:\n")
-almuerzo6 = input("Que vas a querer para almorzar el sabado:\n")
-cena6 = input("Que vas a querer para cenar el sabado:\n")
-almuerzo7 = input("Que vas a querer para almorzar el domingo:\n")
-cena7 = input("Que vas a querer para cenar el domingo:\n")
-print("Perfecto el menu quedaria asi:")
-print("[",dia1,"]", "Almuerzo:", "[",almuerzo1,"]", "Cena:", "[",cena1,"]",)
-time.sleep(0.65)
-print("[",dia2,"]", "Almuerzo:", "[",almuerzo2,"]", "Cena:", "[",cena2,"]",)
-time.sleep(0.65)
-print("[",dia3,"]", "Almuerzo:", "[",almuerzo3,"]", "Cena:", "[",cena3,"]",)
-time.sleep(0.65)
-print("[",dia4,"]", "Almuerzo:", "[",almuerzo4,"]", "Cena:", "[",cena4,"]",)
-time.sleep(0.65)
-print("[",dia5,"]", "Almuerzo:", "[",almuerzo5,"]", "Cena:", "[",cena5,"]",)
-time.sleep(0.65)
-print("[",dia6,"]", "Almuerzo:", "[",almuerzo6,"]", "Cena:", "[",cena6,"]",)
-time.sleep(0.65)
-print("[",dia7,"]", "Almuerzo:", "[",almuerzo7,"]", "Cena:", "[",cena7,"]",)
-
-
-
-
+comida = 0
+dia = 0
+almuerzo = {}
+cena = {}
+semana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+print ("¿Que menu vas a querer esta semana?\n")
+time.sleep(2)
+ for dia in semana:
+ 	comida=input("¿Que quieres para almorzar?")
+ 	almuerzo[dia]=comida
+ 	comida=input("¿Que quieres para cenar?")
+ 	cena[dia]=comida
+ print("Este es el menu para esta semana")
+ time.sleep(2)
+ for dia in semana:
+ 	print("[",dia,"]","-", "Almuerzo:", "[",almuerzo[dia], "Cena:", "[",cena[dia],"]")
+ 	time.sleep(1)
