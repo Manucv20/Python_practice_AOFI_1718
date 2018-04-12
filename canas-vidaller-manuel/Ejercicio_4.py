35fae102501b3e0805d9cc574b7a91d0ea8733a1
Manucv20 <manusevilla20@gmail.com>
2018-04-12 20:35:13 +0200

    añado el ejercicio4

--- canas-vidaller-manuel/Ejercicio_4.py
+++ canas-vidaller-manuel/Ejercicio_4.py
@@ -1,1 +1,15 @@
-print ("Dani es un come huevos/n")
+#Creo e inicializo las variables
+import time
+num = 0
+
+#Pregunto un número para la cuenta atrás
+num = int(input("Dime un número para la cuenta atrás:\n"))
+
+#Se inicia la cuenta atrás
+for i in range(1,num):
+	print (num-i)
+	#Paramos el tiempo 0.5s
+	time.sleep(0.5)
+
+#Indicar que el cohete a despegado
+print ("¡¡El cohete a despegado!!")
